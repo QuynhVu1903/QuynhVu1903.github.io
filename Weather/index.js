ec31ff7715a69b0a3cda93965fa6d03c9949880d
QuynhVu1903 <thuyquynhvu2002@gmail.com>
2026-03-03 11:26:11 +0700

    fix: display location icon and city's name

--- Weather/index.js
+++ Weather/index.js
@@ -8,10 +8,10 @@ async function searchWeather() {
         const response = await fetch(url);
         const data = await response.json();
         if (data.cod !== "200") {
-            alert("Không tìm thấy thành phố");
+            alert("Không tìm thấy thành phố!");
             return;
         }
-        displayWeather(data.list);
+        displayWeather(data.list, data.city);
 
     }
     catch (error) {
@@ -19,7 +19,7 @@ async function searchWeather() {
     }
 }
 
-function displayWeather(list) {
+function displayWeather(list, city) {
     const container = document.getElementById("weatherContainer");
     container.innerHTML = "";
 
@@ -45,22 +45,18 @@ function displayWeather(list) {
         `;
         container.appendChild(card);
     });
-    const iconBox = document.getElementById("place");
-    iconBox.innerHTML = "";
-    // gán Icon
-    const divIcon = document.createElement("div");
-    divIcon.classList.add("divIcon");
-    const icon = document.createElement("i");
-    icon.classList.add("fa-solid", "fa-location-dot");
-    divIcon.appendChild(icon);
-    //gán City
-    const divCity = document.createElement("div");
-    divCity.classList.add("divCity");
-    const cityInput = document.getElementById("inputWeather").value;
-    divCity.textContent = cityInput;
-    // Gộp Icon và City
-    iconBox.appendChild(divIcon);
-    iconBox.appendChild(divCity);
 
-    // xem lại đoạn này
+    //tạo thêm icon và tên thành phố
+    //tên thành phố đã lấy ở function trên nên số liệu có thể bị thay đổi khi chạy
+    // nên lấy data từ mảng
+    const iconBox = document.getElementById("place");
+    iconBox.innerHTML = `
+    <div class="divIcon">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M536.5-503.5Q560-527 560-560t-23.5-56.5Q513-640 480-640t-56.5 23.5Q400-593 400-560t23.5 56.5Q447-480 480-480t56.5-23.5ZM480-186q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg>
+    </div>
+    <div class="divCity">
+        ${city.name}, ${city.country}
+    </div>
+    `;
+
 }
