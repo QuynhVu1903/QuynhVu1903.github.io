90e23a76bea7bd86ae4a328581353d9e5ed2a3d1
QuynhVu1903 <thuyquynhvu2002@gmail.com>
2026-03-23 08:54:02 +0700

    page header

---
+++ my-travelling-website/src/App.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+import { MapPin, Clock, ChevronDown, Search, Ship } from "lucide-react";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="pic1">
+        <div className="headerPic1">
+          <div className="listLeft">
+            <div className="listLeft1">
+              <div className="listLeft1-icon">
+                <MapPin size={15} />
+              </div>
+              <div className="listLeft1-text">
+                72 Hang Trong St., Hoan Kiem Dist., Hanoi, Vietnam 2000
+              </div>
+            </div>
+            <div className="listLeft2">
+              <div className="liftLeft2Icon">
+                <Clock size={15} color="#0D0D0C" />
+              </div>
+              <div className="liftLeft2Text">Hotline: +84-946 505 505</div>
+            </div>
+          </div>
+          <div className="listRight">
+            <div className="borderButton">
+              <div>Language</div>
+              <div className="buttonDown">
+                <ChevronDown size={15} color="#0D0D0C" />
+              </div>
+            </div>
+            <div className="frame305">
+              <div className="frame3051">FAQ</div>
+              <div className="frame3052">Support</div>
+              <div>Sign in/ Register</div>
+            </div>
+          </div>
+        </div>
+        <div className="backgroundPic1">
+          <div className="backgroundPic1Logo">
+            <img src="/logo.png" />
+          </div>
+          <div className="backgroundPic1-nav">
+            <a className="firstA" href="#">
+              Home
+            </a>
+            <a href="#">Halong Bay Cruise Packages</a>
+            <a href="#">Cat Ba - Lan Ha Cruise Packages</a>
+            <a href="#">Why us</a>
+          </div>
+          <div className="backgroundPic1-link">
+            <a href="#">Book A Tour</a>
+          </div>
+        </div>
+        <div className="contentPic1">
+          <div className="frame337">
+            <div className="frame322">
+              <div className="frame322-up">
+                Unforgettable Halong Bay Cruises
+              </div>
+              <div className="frame322-down">
+                with Asia Pacific Travel - Since 2002
+              </div>
+            </div>
+            <div className="frame11">
+              <div className="frame610">
+                {/* gồm search và cruise */}
+                <div className="frame6">
+                  <div className="frame6-icon">
+                    <Search />
+                  </div>
+                  <div className="frame6-text">Search destinations, hotels</div>
+                </div>
+                <div className="frame10">
+                  <div className="frame10-icon">
+                    <Ship />
+                  </div>
+                  <div className="frame10-text">
+                    Cruise category</div>
+                </div>
+              </div>
+              <div className="coolection-button">Search</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
