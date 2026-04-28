264a508755d757bede3a1b71cf391235a5ec2238
QuynhVu1903 <thuyquynhvu2002@gmail.com>
2026-04-28 17:17:43 +0700

    chore: create user and use post API

--- my-app/src/components/user/user.form.tsx
+++ my-app/src/components/user/user.form.tsx
@@ -1,14 +1,30 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
+import { createUserAPI } from "../../services/api.service";
+
 const UserForm = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+  const [phone, setPhone] = useState("");
 
-  const handleClickBtn = () => {
-    
-  }
+  const [api, contextHolder] = notification.useNotification();
+
+  const handleClickBtn = async () => {
+    const res = await createUserAPI(fullName, email, password, phone);
+    console.log("check res backend: ", res)
+    if (res.data) {
+      // nếu biến res.data tồn tại thì check
+      //tiếp res.data.data có tồn tại hay không
+      //dùng antd, nếu có data trả về thì bắn ra thông báo:
+      api.success({
+        message: "create user",
+        description: "Tạo user thành công",
+      });
+    }
+
+    console.log("check res react: ", res.data);
+  };
   return (
     <div
       style={{
@@ -16,6 +32,7 @@ const UserForm = () => {
       }}
       className="user-form"
     >
+      {contextHolder}
       <div
         style={{
           gap: "15px",
@@ -26,32 +43,43 @@ const UserForm = () => {
         <div>
           <span>FullName</span>
           <Input
-          value={fullName} 
-          onChange={(event)=> {setFullName(event.target.value)}}
+            value={fullName}
+            onChange={(event) => {
+              setFullName(event.target.value);
+            }}
           />
         </div>
         <div>
           <span>Email</span>
           <Input
-          value={email}
-          onChange={(event) => {setEmail(event.target.value)}} />
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Password</span>
-          <Input.Password 
-          value={password}
-          onChange={(event)=> {setPassword(event.target.value)}}/>
+          <Input.Password
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Phone number</span>
-          <Input 
-          value={phoneNumber}
-          onChange={(event)=>{setPhoneNumber(event.target.value)}}/>
+          <Input
+            value={phone}
+            onChange={(event) => {
+              setPhone(event.target.value);
+            }}
+          />
         </div>
         <div>
-          <Button type="primary"
-          onClick={handleClickBtn}
-          >Create User</Button>
+          <Button type="primary" onClick={handleClickBtn}>
+            Create User
+          </Button>
         </div>
       </div>
     </div>
