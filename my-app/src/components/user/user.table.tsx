1df84cf34d586e7c3f35d0cd32271b037d3569ea
QuynhVu1903 <thuyquynhvu2002@gmail.com>
2026-05-07 16:56:28 +0700

    chore: automatically update new user in the table

--- my-app/src/components/user/user.table.tsx
+++ my-app/src/components/user/user.table.tsx
@@ -1,5 +1,5 @@
-import { Space, Table, Tag } from 'antd';
-import type { TableProps } from 'antd';
+import { Table } from "antd";
+import type { TableProps } from "antd";
 
 interface DataType {
   key: string;
@@ -8,87 +8,33 @@ interface DataType {
   address: string;
   tags: string[];
 }
-
-
-
-// const App: React.FC = () => 
-
-const UserTable = () => {
-const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
-
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
-
-return (
-    <Table<DataType> columns={columns} dataSource={data} />
-)
+type Dprops = {
+  dataUsers: never[]
 }
-export default UserTable
+
+const UserTable = (props: Dprops) => {
+  const { dataUsers } = props;
+
+  const columns: TableProps<DataType>["columns"] = [
+    {
+      title: "Id",
+      dataIndex: "_id", //name: key
+    },
+    {
+      title: "Full Name",
+      dataIndex: "fullName",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+  ];
+
+  
+
+  console.log("run render 000");
+  return (
+    <Table<DataType> columns={columns} dataSource={dataUsers} rowKey={"_id"} />
+  );
+};
+export default UserTable;
