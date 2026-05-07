c9cdbf4bd7afb335d876bb42bfcb259dbd3ef637
QuynhVu1903 <thuyquynhvu2002@gmail.com>
2026-05-07 16:57:05 +0700

    using lifting up state

--- my-app/src/pages/users.tsx
+++ my-app/src/pages/users.tsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react";
 import UserForm from "../components/user/user.form";
 import UserTable from "../components/user/user.table";
+import { fetchAllUserAPI } from "../services/api.service";
 
 const UsersPage = () => {
-    return (
+  const [dataUsers, setDataUsers] = useState([]);
 
-            <div style={{padding: "20px"}}>
-                <UserForm />
-                <UserTable />
-            </div>
+  useEffect(() => {
+    console.log("run useEffect 111");
+    loadUser(); // chỉ chạy 1 lần
+  }, []);
 
-    )
-}
+  const loadUser = async () => {
+    const res = await fetchAllUserAPI(); //chờ fetchAllUserAPI thực hiện xong rồi trả vào biến res
+    setDataUsers(res.data);
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <UserForm loadUser={loadUser} />
+      <UserTable dataUsers={dataUsers} />
+    </div>
+  );
+};
 export default UsersPage;
